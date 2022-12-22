034b5c749b1a40f432a4d42c381b1da2a8f0c32e
Douglas Shibata <douglas.shibata11@gmail.com>
2022-12-22 17:38:03 -0300

    Refatorando código

--- src/server-socket.js
+++ src/server-socket.js
@@ -3,6 +3,10 @@ import io from "./server.js";
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou!");
     socket.on("texto_editor", (texto) => {
-        console.log(texto);
+        socket.broadcast.emit("texto_editor_clientes", texto);
+    });
+    socket.on("disconnect", (motivo) => {
+        console.log(`Cliente "${socket.id}" desconectado!
+        Motivo: ${motivo}`);
     });
 });
