be3af1e9af37304119a61f22892d1f29547bfa18
Douglas Shibata <douglas.shibata11@gmail.com>
2022-12-22 17:11:08 -0300

    Adicionando socket io, e testando conexão

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # alura-docs-websocket-mongodb
 Curso da Alura de WebSockets: implemente comunicações em tempo real com Socket.IO e MongoDB
+
+
+## Dependencias
+* Express
+* Socket.io
+
+
+## Como usar ?
+Verifique se tem o nodejs instalado
+```bash
+node -v
+```
+Depois instalar as dependencias do projeto com o comando
+```bash
+npm install
+```
+
+
+## Executando o projeto
+Para executar o projeto com o nodemon
+```bash
+npm run dev
+```
+Para executar o projeto com o node
+```bash
+npm start
+```
+
+### 
+Após rodar o projeto 
+abrir o navegador em http://localhost:3000
--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import path from 'path';
 import url from 'url';
+import { Server } from "socket.io";
+import http from 'http';
 
 const app = express();
-
 const porta = process.env.porta || 3000;
 
 const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 app.use(express.static(diretorioPublico));
-app.listen(porta, () => console.log(`Servidor Rodando na porta: ${porta}`));
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(porta, () => console.log(`Servidor escutando na porta ${porta}`));
+const io = new Server(servidorHttp);
+io.on("connection", () => {
+    console.log("Um cliente se conectou!");
+});
