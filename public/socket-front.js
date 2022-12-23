d734cef85faf132daa37e9c07e9f3dcfc938d3e0
Douglas Shibata <douglas.shibata11@gmail.com>
2022-12-23 16:49:45 -0300

    Comunicação entre os documentos e armazenamento da informação em tempo de execução

--- public/socket-front.js
+++ public/socket-front.js
@@ -2,6 +2,12 @@ import { atualizaTextoEditor } from "./documento.js";
 
 const socket = io();
 
+function selecionarDocumento(nome) {
+    socket.emit("selecionar_documento", nome, (texto) => {
+        atualizaTextoEditor(texto);
+    });
+}
+
 function emitirTextoEditor(dados) {
     socket.emit("texto_editor", dados);
 }
@@ -9,9 +15,5 @@ function emitirTextoEditor(dados) {
 socket.on("texto_editor_clientes", (texto) => {
     atualizaTextoEditor(texto);
 });
-
-function selecionarDocumento(nome) {
-    socket.emit("selecionar_documento", nome);
-}
 
 export { emitirTextoEditor, selecionarDocumento };
